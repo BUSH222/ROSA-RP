0599cad41f5089bd537a4a4b5a93b6fa0a44ddd5
Ted Vtorov <58144503+BUSH222@users.noreply.github.com>
2026-09-21 12:21:12 +0300

    final path change in default settings

--- config/settings.py
+++ config/settings.py
@@ -87,9 +87,11 @@ class Secrets(BaseSettings):
 class SatdumpSettings(BaseSettings):
     """Settings for the Satdump application."""
 
-    satdump_path_str: str = Field(default="~/GobDump/", env="SATDUMP_PATH")
-    satdump_run_path_str: str = Field(default="~/GobDump/build/", env="SATDUMP_RUN_PATH")
-    satdump_database_path_str: str = Field(default="~/.config/gobdump/main.db", env="SATDUMP_DATABASE_PATH")
+    satdump_path_str: str = Field(default="/home/orangepi/GobDump/", env="SATDUMP_PATH")
+    satdump_run_path_str: str = Field(default="/home/orangepi/GobDump/build/", env="SATDUMP_RUN_PATH")
+    satdump_database_path_str: str = Field(
+        default="/home/orangepi/.config/gobdump/main.db", env="SATDUMP_DATABASE_PATH"
+    )
 
 
 class Settings(BaseSettings):
